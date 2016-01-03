fa8a2411819bcec5373547f15105b2473ceb70eb
Matthew Loberg <loberg.matt@gmail.com>
2016-01-03 11:47:38 -0600

    Added ProcessorResolver
    Add a resolver for processors that will return a processor that supports the resource

---
+++ src/Processor/ProcessorResolver.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Mlo\Babl\Processor;
+
+class ProcessorResolver
+{
+    /**
+     * @var ProcessorInterface[]|array
+     */
+    private $processors;
+
+    /**
+     * Constructor
+     *
+     * @param array|ProcessorInterface[] $processors
+     */
+    public function __construct(array $processors = [])
+    {
+        $this->processors = [];
+
+        foreach ($processors as $processor) {
+            $this->addProcessor($processor);
+        }
+    }
+
+    /**
+     * Returns a processor that supports given extension
+     *
+     * @param string $format
+     *
+     * @return ProcessorInterface|bool false if no supported processor found
+     */
+    public function resolve($format)
+    {
+        foreach ($this->processors as $processor) {
+            if ($processor->supports($format)) {
+                return $processor;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Add processor to resolver
+     *
+     * @param ProcessorInterface $processor
+     *
+     * @return ProcessorResolver
+     */
+    public function addProcessor(ProcessorInterface $processor)
+    {
+        $this->processors[] = $processor;
+
+        return $this;
+    }
+
+    /**
+     * Returns the registered processors
+     *
+     * @return array|ProcessorInterface[]
+     */
+    public function getProcessors()
+    {
+        return $this->processors;
+    }
+}
