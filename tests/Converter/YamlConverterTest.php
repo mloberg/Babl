f63184067375d08b687f7c500a9bfd3a54521603
Matthew Loberg <loberg.matt@gmail.com>
2016-01-19 17:54:01 -0600

    Add YamlConverterTest

---
+++ tests/Converter/YamlConverterTest.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * Copyright (c) 2016 Matthew Loberg
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
+
+namespace Mlo\Babl\Tests\Converter;
+
+use Mlo\Babl\Converter\YamlConverter;
+
+/**
+ * @coversDefaultClass \Mlo\Babl\Converter\YamlConverter
+ */
+class YamlConverterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var YamlConverter
+     */
+    private $converter;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp()
+    {
+        $this->converter = new YamlConverter();
+    }
+
+    /**
+     * @covers ::convert
+     */
+    public function testConvert()
+    {
+        $data = ['foo' => 'bar'];
+        $expected = <<<EOF
+foo: bar
+
+EOF;
+
+        $this->assertEquals($expected, $this->converter->convert(new \ArrayIterator($data)));
+    }
+
+    /**
+     * @covers ::supports
+     */
+    public function testSupports()
+    {
+        $this->assertTrue($this->converter->supports('yaml'));
+        $this->assertTrue($this->converter->supports('YAML'));
+        $this->assertTrue($this->converter->supports('yml'));
+        $this->assertTrue($this->converter->supports('YML'));
+        $this->assertFalse($this->converter->supports('xliff'));
+        $this->assertFalse($this->converter->supports('xlf'));
+    }
+
+    /**
+     * @covers ::getExtension
+     */
+    public function testGetExtension()
+    {
+        $this->assertEquals('yml', $this->converter->getExtension());
+    }
+}
