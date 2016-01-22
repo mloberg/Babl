fd8933b339954ffb8dc661f985f9a8be6c6185de
Matthew Loberg <loberg.matt@gmail.com>
2016-01-21 23:10:51 -0600

    Add tests for ProcessorResolver

---
+++ tests/Mock/ProcessorMock.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Copyright (c) 2016 Matthew Loberg
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
+
+namespace Mlo\Babl\Tests\Mock;
+
+use Mlo\Babl\Processor\ProcessorInterface;
+
+class ProcessorMock implements ProcessorInterface
+{
+    /**
+     * @var string
+     */
+    private $format;
+
+    /**
+     * Constructor
+     *
+     * @param string $format
+     */
+    public function __construct($format = 'php')
+    {
+        $this->format = $format;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process($file)
+    {
+        // TODO: Implement process() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supports($format)
+    {
+        return $format === $this->format;
+    }
+}
---
+++ tests/Processor/ProcessorResolverTest.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * Copyright (c) 2016 Matthew Loberg
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
+
+namespace Mlo\Babl\Tests\Processor;
+
+use Mlo\Babl\Processor\ProcessorResolver;
+use Mlo\Babl\Tests\Mock\ProcessorMock;
+
+/**
+ * @coversDefaultClass \Mlo\Babl\Processor\ProcessorResolver
+ * @covers ::__construct
+ */
+class ProcessorResolverTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ProcessorResolver
+     */
+    private $resolver;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp()
+    {
+        $this->resolver = new ProcessorResolver([new ProcessorMock('php')]);
+    }
+
+    /**
+     * @covers ::addProcessor
+     * @covers ::getProcessors
+     */
+    public function testAddGetProcessors()
+    {
+        $this->assertCount(1, $this->resolver->getProcessors());
+        $this->assertFalse($this->resolver->getProcessors()[0]->supports('yml'));
+        $this->assertSame($this->resolver, $this->resolver->addProcessor(new ProcessorMock('yml')));
+        $this->assertCount(2, $this->resolver->getProcessors());
+        $this->assertFalse($this->resolver->getProcessors()[0]->supports('yml'));
+        $this->assertTrue($this->resolver->getProcessors()[1]->supports('yml'));
+    }
+
+    /**
+     * @covers ::resolve
+     */
+    public function testResolve()
+    {
+        $processor = new ProcessorMock('xliff');
+
+        $this->assertFalse($this->resolver->resolve('xliff'));
+        $this->resolver->addProcessor($processor);
+        $this->assertSame($processor, $this->resolver->resolve('xliff'));
+    }
+}
