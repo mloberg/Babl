83dc0cba3a62affa38b77d7bcc071ac295122fea
Matthew Loberg <loberg.matt@gmail.com>
2016-01-24 18:17:03 -0600

    Add tests for YamlProcessor

---
+++ tests/Processor/YamlProcessorTest.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Copyright (c) 2016 Matthew Loberg
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
+
+namespace Mlo\Babl\Tests\Processor;
+
+use Mlo\Babl\Processor\YamlProcessor;
+
+/**
+ * @coversDefaultClass \Mlo\Babl\Processor\YamlProcessor
+ */
+class YamlProcessorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var YamlProcessor
+     */
+    private $processor;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp()
+    {
+        $this->processor = new YamlProcessor();
+    }
+
+    /**
+     * @covers ::process
+     */
+    public function testProcess()
+    {
+        $data = $this->processor->process(__DIR__ . '/../data/messages.en.yml');
+
+        $this->assertInstanceOf('Traversable', $data);
+        $this->assertEquals('bar', $data['foo']);
+    }
+
+    /**
+     * @covers ::supports
+     */
+    public function testSupports()
+    {
+        $this->assertTrue($this->processor->supports('yaml'));
+        $this->assertTrue($this->processor->supports('YAML'));
+        $this->assertTrue($this->processor->supports('yml'));
+        $this->assertTrue($this->processor->supports('YML'));
+        $this->assertFalse($this->processor->supports('php'));
+        $this->assertFalse($this->processor->supports('xliff'));
+    }
+}
